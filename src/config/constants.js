ad625bee80f9c667bd1afd04cc9a5b5b00261712
tommardev <tommardev@gmail.com>
2026-09-11 14:57:30 +0300

    chore: bump version to v1.3.0 and refresh asset cache busters

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,5 +3,5 @@
  */
 export const LOCAL_STORAGE_KEY = "tautos_bukiausias_v3_state";
 export const MAX_SELECTED_CANDIDATES = 3;
-export const APP_VERSION = "v1.2.1";
+export const APP_VERSION = "v1.3.0";
 
